3cfaa89599838c26a2f7d44d3b35b783ab47da8c
Andrii Zakopets <snailz839@gmail.com>
2021-09-05 00:24:14 +0300

    add express

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
+import express from 'express';
 import fs from 'fs';
 // import { getItemsBitskins } from './api.js';
 import { Item } from './models.js';
 import { getItemNameid } from './lib.js';
 
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.listen(80);
 mongoose.connect('mongodb+srv://admin:admin@cluster0.voepl.mongodb.net/items');
 
 // const resp = await getItemsBitskins('123456');
@@ -12,16 +18,16 @@ mongoose.connect('mongodb+srv://admin:admin@cluster0.voepl.mongodb.net/items');
 const names = JSON.parse(fs.readFileSync('names.json').toString());
 
 for (const name of names) {
-    const saved = await Item.find({ market_hash_name: name });
-    const isSaved = saved.length > 0;
-    if (!isSaved) {
-        const id = await getItemNameid(name);
-        console.log(name, id);
+  const saved = await Item.find({ market_hash_name: name });
+  const isSaved = saved.length > 0;
+  if (!isSaved) {
+    const id = await getItemNameid(name);
+    console.log(name, id);
 
-        const item = new Item({
-            market_hash_name: name,
-            item_nameid: id,
-        });
-        item.save();
-    }
+    const item = new Item({
+      market_hash_name: name,
+      item_nameid: id,
+    });
+    item.save();
+  }
 }
