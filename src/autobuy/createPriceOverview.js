842cf5f6729857bc79e83c3db022c81b36072ac0
Andrii Zakopets <snailz839@gmail.com>
2021-11-01 02:00:32 +0200

    update priceoverview

--- src/autobuy/createPriceOverview.js
+++ src/autobuy/createPriceOverview.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { sleep } from '../utils.js';
 import { getPriceOverview } from '../api.js';
 import Bottleneck from 'bottleneck';
@@ -8,7 +9,7 @@ const limitedGetPriceOverview = limiter.wrap((name) => getPriceOverview(name));
 const retryPriceOverview = async function repeat(callback, name) {
   console.log(`getting price for ${name}`);
   try {
-    const {data: response} = await callback(name);
+    const { data: response } = await callback(name);
 
     if (!(response.success && response.lowest_price && response.volume && response.median_price))
       throw 1;
@@ -23,18 +24,28 @@ const retryPriceOverview = async function repeat(callback, name) {
 
 export const createPriceOverview = function () {
   const parsePrice = (price = '') => parseFloat(price.replace(',', '.'));
-  const priceOverview = {};
+  let priceOverview = {};
+  try {
+    priceOverview = JSON.parse(fs.readFileSync('./src/autobuy/priceOverview.json'));
+  } catch (e) {}
 
   return async (name) => {
-    if (!priceOverview[name]) {
+    const timestamp = new Date().getTime();
+
+    if (!priceOverview[name] || timestamp - priceOverview[name].timestamp > 1000 * 60 * 60 * 10) {
       const newPrice = await retryPriceOverview(limitedGetPriceOverview, name);
-      console.log(`new priceOverview (${Object.keys(priceOverview).length}) ${name}: ${newPrice.lowest_price}`);
+      console.log(
+        `new priceOverview (${Object.keys(priceOverview).length}) ${name}: ${newPrice.lowest_price}`
+      );
 
       priceOverview[name] = {
         lowestPrice: parsePrice(newPrice.lowest_price),
         medianPrice: parsePrice(newPrice.median_price),
         volume: parseFloat(newPrice.volume),
+        timestamp,
       };
+
+      fs.writeFileSync('./src/autobuy/priceOverview.json', JSON.stringify(priceOverview));
     }
 
     return priceOverview[name];
